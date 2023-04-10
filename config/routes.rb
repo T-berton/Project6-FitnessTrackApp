9bd4715d626cb90dafcd05c3d2daa5684c6f2029
T-berton <tberton@etud.insa-toulouse.fr>
2023-04-10 17:28:57 +0200

    new_version without edit and new on activities

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home', :as => :home
-  get '/activity/index' => 'activities#index', :as => :index
-  get '/activity/new' => 'activities#new', :as => :new
-  post '/activity/create' => 'activities#create', :as => :activities
-  put '/activity/update/:id' => 'activities#update', :as => :update_activity
+  get '/activity/index' => 'activities#index', :as => :index 
   get '/activity/show/:id' => 'activities#show', :as => :show
-  get '/activity/edit/:id' =>'activities#edit', :as => :edit
 
   get '/myactivity/index' => 'activity_users#index', :as => :activity_user_index
   get '/myactivity/new' => 'activity_users#new', :as => :activity_user_new
